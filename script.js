df2ef7ad617da879940767798c8144787b68752a
Ana Glória <anaparente196@gmail.com>
2024-03-26 23:29:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function calcular () {
+function calcular() {
     let num = document.getElementById('inum')
-    
-}
+    let tab= document.getElementById('itabuada')
+
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+
+        for(let c = 1; )
+    }
+
+
+
+    }
